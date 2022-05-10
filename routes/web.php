5313c676ac2ed337839bd5d98680b095827c6030
kassahundinber <kassahun1722@gmail.com>
2022-05-10 16:18:50 +0300

    catagory  added to Ecommerce

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Models\products;
+use App\Models\Catagory;
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\productscontroller;
+use App\Http\controllers\Catagorycontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,11 +22,14 @@ Route::get('/', function () {
 });
 Route::get('/products/register', [ProductsController::class, 'register'])->name('products/register');
 Route::post('/products/register', [ProductsController::class, 'store'])->name('products/register');
+Route::get('/Catagory/register', [CatagoryController::class, 'register'])->name('Catagory/register');
+Route::post('/Catagory/register', [CatagoryController::class, 'store'])->name('Catagory/register');
 
 
 Auth::routes();
 Route::get('/home', [App\Http\controllers\HomeControllers ::class, 'index'])->name('home');
 Route::get('/products/list', [ProductsController::class, 'get_all'])->name('products/list');
+Route::get('/Catagory/list', [CatagoryController::class, 'get_all'])->name('Catagory/list');
 Route::get('/products/edit/{id}', [ProductsController::class, 'edit']);
 Route::post('/products/update',[ProductsController::class,'update'])->name('products/update');
 Route::get('/products/delete/{id}', [ProductsController::class, 'delete']);
